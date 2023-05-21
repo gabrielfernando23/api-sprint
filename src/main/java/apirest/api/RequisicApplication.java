fb81d83f626c252f8bcf0c6487fcbc1e6581490e
gabrielfernando23 <gabriel.ssilva@sptech.school>
2023-05-21 01:16:01 -0300

    att

---
+++ src/main/java/apirest/api/RequisicApplication.java
@@ -0,0 +1,13 @@
+package apirest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequisicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequisicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apirest/api/RequisicApplicationTests.java
@@ -0,0 +1,13 @@
+package apirest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequisicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
